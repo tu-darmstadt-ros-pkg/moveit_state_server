79d749c1bfee97603de4b51521809b1fffcf9993
Aljoscha Schmidt <aljoscha97@web.de>
2023-03-16 17:08:19 +0100

    reformatting

--- moveit_state_server/include/moveit_state_server/moveit_state_server.h
+++ moveit_state_server/include/moveit_state_server/moveit_state_server.h
@@ -15,57 +15,66 @@
 #include <moveit/moveit_cpp/planning_component.h>
 
 
-namespace moveit_state_server
-{
-inline
-    void convert(const geometry_msgs::Transform& trans, geometry_msgs::PoseStamped& pose, const std::string& frame)
-{
-  pose.header.frame_id = frame;
-  pose.pose.orientation = trans.rotation;
-  pose.pose.position.x = trans.translation.x;
-  pose.pose.position.y = trans.translation.y;
-  pose.pose.position.z = trans.translation.z;
-}
+namespace moveit_state_server {
+    inline
+    void convert(const geometry_msgs::Transform &trans, geometry_msgs::PoseStamped &pose, const std::string &frame) {
+        pose.header.frame_id = frame;
+        pose.pose.orientation = trans.rotation;
+        pose.pose.position.x = trans.translation.x;
+        pose.pose.position.y = trans.translation.y;
+        pose.pose.position.z = trans.translation.z;
+    }
 
-class MoveitStateServer
-{
-public:
+    class MoveitStateServer {
+    public:
 
-  MoveitStateServer(ros::NodeHandle &pnh);
-private:
-  bool storePoseService( moveit_state_server_msgs::StorePoseRequest &req,
-                         moveit_state_server_msgs::StorePoseResponse &res );
-  bool retrievePoseService( moveit_state_server_msgs::RetrievePoseRequest &req,
-                         moveit_state_server_msgs::RetrievePoseResponse &res );
-  void goalCB(const moveit_state_server_msgs::GoToStoredStateGoalConstPtr &goal);
-  void preemptCB();
-  bool switchController(bool to_tcp);
-  void storeCurrentJointStates(const std::string& name);
-  void storeCurrentPose(const std::string& name);
-    void go_to_stored_joint_state(const std::string& name);
-    void go_to_stored_eef_position(const std::string& name);
-    bool check_if_pose_exists(const std::string& name);
-    bool check_if_joint_states_exists(const std::string& name);
-  ros::NodeHandle nh_;
-  ros::NodeHandle pnh_;
-  std::string planning_group_ = "arm_group";
-  std::string position_reference_frame_ = "world";
-  ros::ServiceServer store_pose_service;
-  ros::ServiceServer retrieve_pose_server;
-  robot_model::RobotModelPtr moveit_robot_model_;
-  actionlib::SimpleActionServer<moveit_state_server_msgs::GoToStoredStateAction> as_;
-  std::shared_ptr<moveit_cpp::MoveItCpp> moveit_cpp_ptr_;
-  std::shared_ptr<moveit_cpp::PlanningComponent> planning_components_;
-  ros::ServiceClient get_planning_scene_;
-  ros::ServiceClient switch_controllers_;
-  std::map<std::string, std::vector<double>> joint_states_;
-  std::string end_effector_;
-  std::string store_pose_service_name_;
-  std::vector<std::string> joint_names_;
-  bool stored_joint_positions_=false;
-  bool stored_end_effector_position_=false;
-    std::map<std::string,geometry_msgs::PoseStamped> poses_;
-private:
-};
+        MoveitStateServer(ros::NodeHandle &pnh);
+
+    private:
+        bool storePoseService(moveit_state_server_msgs::StorePoseRequest &req,
+                              moveit_state_server_msgs::StorePoseResponse &res);
+
+        bool retrievePoseService(moveit_state_server_msgs::RetrievePoseRequest &req,
+                                 moveit_state_server_msgs::RetrievePoseResponse &res);
+
+        void goalCB(const moveit_state_server_msgs::GoToStoredStateGoalConstPtr &goal);
+
+        void preemptCB();
+
+        bool switchController(bool to_tcp);
+
+        void storeCurrentJointStates(const std::string &name);
+
+        void storeCurrentPose(const std::string &name);
+
+        void go_to_stored_joint_state(const std::string &name);
+
+        void go_to_stored_eef_position(const std::string &name);
+
+        bool check_if_pose_exists(const std::string &name);
+
+        bool check_if_joint_states_exists(const std::string &name);
+
+        ros::NodeHandle nh_;
+        ros::NodeHandle pnh_;
+        std::string planning_group_ = "arm_group";
+        std::string position_reference_frame_ = "world";
+        ros::ServiceServer store_pose_service;
+        ros::ServiceServer retrieve_pose_server;
+        robot_model::RobotModelPtr moveit_robot_model_;
+        actionlib::SimpleActionServer<moveit_state_server_msgs::GoToStoredStateAction> as_;
+        std::shared_ptr<moveit_cpp::MoveItCpp> moveit_cpp_ptr_;
+        std::shared_ptr<moveit_cpp::PlanningComponent> planning_components_;
+        ros::ServiceClient get_planning_scene_;
+        ros::ServiceClient switch_controllers_;
+        std::map<std::string, std::vector<double>> joint_states_;
+        std::string end_effector_;
+        std::string store_pose_service_name_;
+        std::vector<std::string> joint_names_;
+        bool stored_joint_positions_ = false;
+        bool stored_end_effector_position_ = false;
+        std::map<std::string, geometry_msgs::PoseStamped> poses_;
+    private:
+    };
 }
 #endif //MOVEIT_STATE_SERVER_H
